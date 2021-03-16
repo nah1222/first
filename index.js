0666a720cac6efdd7b15d251593ca9124ac5c5b0
nah1222 <nah1222@yahoo.com>
2021-03-16 16:50:43 -0400

    Frequency

--- index.js
+++ index.js
@@ -66,13 +66,19 @@ console.log(findPermutation("god"))
 //Program to print elements of an string present on odd position, along with the count of those elements in the entire string
 //i.e abbloseckc - a:1,b:2,o:1,e:1,k:1 -------------***
 function frequency(str){
-    let counter = [];
-    const letters = str.split("")
-    for(let i=0; i<letters.length; i++){
-    counter[str[i]]?counter[str[i]]++: counter[str[i]] = 1 ;
+    let counter = {};
+    
+    for(let i=0; i<str.length; i++){
+        const char = str.charAt(i);
+        if (counter[char]){
+            counter[char]++;
+        } else {
+            counter[char] = 1;
+        }
     }
     return counter;
-}
+};
+console.log(frequency("dsjkfhlakjhreinfw"))
 //Write a function that given a list of countries, returns the country(s) that occur the least times
 //i.e - ["Argentina", "Cuba", "Chile", "Argentina"] -> ["Cuba", "Chile"] -----------***
 function recurrence(arr){
